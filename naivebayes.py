076cd97e213f0b2fd80ce864ef88c126383c2398
Owen <owendot2011@gmail.com>
2022-11-30 22:23:30 -0500

    refactoring

--- naivebayes.py
+++ naivebayes.py
@@ -2,22 +2,24 @@ import multiprocessing
 from functools import partial
 
 import numpy as np
-
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
+
 
 
 class NaiveBayesClassifer(ClassifierMixin, BaseEstimator):
     """ A classifier which implements a naive bayes algorithm.
     Attributes
     ----------
-    X_ : ndarray, shape (n_samples, n_features)
-        The input passed during :meth:`fit`.
-    y_ : ndarray, shape (n_samples,)
-        The labels passed during :meth:`fit`.
-    classes_ : ndarray, shape (n_classes,)
-        The classes seen at :meth:`fit`.
+    class_prior_ : ndarray of shape (n_classes,)
+        probability of each class.
+
+    classes_ : ndarray of shape (n_classes,)
+        class labels known to the classifier.
+
+    data_: ndarray of shape (n_samples, n_features + 1)
+        matrix containing input X with y stacked as the last column
     """
 
     def fit(self, X, y):
@@ -37,9 +39,9 @@ class NaiveBayesClassifer(ClassifierMixin, BaseEstimator):
         X, y = check_X_y(X, y)
         # Store the classes seen during fit
         self.classes_ = unique_labels(y)
+        self.class_priors_ = np.array([len(y[y == cls]) for cls in self.classes_])
+        self.data_ = np.column_stack((X, y))
 
-        self.X_ = X
-        self.y_ = y
         # Return the classifier
         return self
 
@@ -58,33 +60,29 @@ class NaiveBayesClassifer(ClassifierMixin, BaseEstimator):
             https://en.wikipedia.org/wiki/Naive_Bayes_classifier#Constructing_a_classifier_from_the_probability_model
         """
         # Check is fit had been called
-        check_is_fitted(self, ['X_', 'y_'])
+        check_is_fitted(self, ['classes_', 'class_priors_', 'data_'])
 
         # Input validation
         X = check_array(X)
-
-        func = partial(self._bayes_classification, X=self.X_, y=self.y_, classes=self.classes_)
+        func = partial(self._bayes_classification, data=self.data_, classes=self.classes_, class_priors=self.class_priors_)
         with multiprocessing.Pool() as p:
             pred = p.map(func, X)
         return pred
 
     @staticmethod
-    def _bayes_classification(x, X, y, classes):
-        # merge training data with labels
-        data = np.column_stack((X, y))
+    def _bayes_classification(X, data, classes, class_priors):
         class_probs = np.array([])
-        # get probability for each class
-        for cls in classes:
+        for cls, cls_prior in zip(classes, class_priors):
             # bitmask for the rows in this class
             class_eq_bitmask = data[:, -1] == cls
             # P(cls)
-            prob = len(data[class_eq_bitmask]) / len(data)
-            for feature in np.arange(len(x)):
+            prob = cls_prior
+            for feature in np.arange(len(X)):
                 # P(x[feature] | cls)
                 prob_ = (
                     len(data[
                         (class_eq_bitmask) &
-                        (data[:, feature] == x[feature])  # rows where class and feature value match
+                        (data[:, feature] == X[feature])  # rows where class and feature value match
                     ])
                     /
                     len(data[class_eq_bitmask])
--- naivebayes_test.py
+++ naivebayes_test.py
@@ -2,44 +2,41 @@ import math
 import unittest
 
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.naive_bayes import GaussianNB
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import KBinsDiscretizer
 from sklearn.metrics import accuracy_score
+from sklearn.dummy import DummyClassifier
+from memory_profiler import profile
 
 
 import naivebayes
 
 
 class TestNaiveBayesClassifier(unittest.TestCase):
-    def test_predict_breast_cancer(self):
-        X, y = datasets.load_breast_cancer(return_X_y=True)
+    def test_wine_dummy(self):
+        X, y = datasets.load_wine(return_X_y=True)
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.2)
 
-        t = KBinsDiscretizer(n_bins=2, encode='onehot-dense', strategy='kmeans')
-        X_train = t.fit_transform(X_train)
-        X_test = t.transform(X_test)
-
-        c = naivebayes.NaiveBayesClassifer()
+        c = DummyClassifier(strategy='uniform')
         c.fit(X_train, y_train)
         y_pred = c.predict(X_test)
-        print("BC Accuracy : ", accuracy_score(y_test, y_pred))
+        print("Dummy Wine Accuracy: ", accuracy_score(y_test, y_pred))
 
-    def test_iris(self):
-        X, y = datasets.load_iris(return_X_y=True)
+    def test_wine_gausian(self):
+        X, y = datasets.load_wine(return_X_y=True)
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.2)
-        t = KBinsDiscretizer(n_bins=3, encode='onehot-dense', strategy='kmeans')
-        X_train = t.fit_transform(X_train)
-        X_test = t.transform(X_test)
 
-        c = naivebayes.NaiveBayesClassifer()
+        c = GaussianNB()
         c.fit(X_train, y_train)
         y_pred = c.predict(X_test)
-        print("Iris Accuracy : ", accuracy_score(y_test, y_pred))
+        print("GaussianNB Wine Accuracy: ", accuracy_score(y_test, y_pred))
 
-    def test_wine(self):
+    def test_wine_probabilistic(self):
         X, y = datasets.load_wine(return_X_y=True)
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.2)
@@ -51,7 +48,27 @@ class TestNaiveBayesClassifier(unittest.TestCase):
         c = naivebayes.NaiveBayesClassifer()
         c.fit(X_train, y_train)
         y_pred = c.predict(X_test)
-        print("Wine Accuracy : ", accuracy_score(y_test, y_pred))
+        print("Wine Accuracy: ", accuracy_score(y_test, y_pred))
+
+    def test_digits_dummy(self):
+        X, y = datasets.load_digits(return_X_y=True)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2)
+
+        c = DummyClassifier(strategy='uniform')
+        c.fit(X_train, y_train)
+        y_pred = c.predict(X_test)
+        print("Dummy Digits Accuracy: ", accuracy_score(y_test, y_pred))
+
+    def test_digits_gausian(self):
+        X, y = datasets.load_digits(return_X_y=True)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2)
+
+        c = GaussianNB()
+        c.fit(X_train, y_train)
+        y_pred = c.predict(X_test)
+        print("GaussianNB Digits Accuracy: ", accuracy_score(y_test, y_pred))
 
     def test_digits(self):
         X, y = datasets.load_digits(return_X_y=True)
@@ -61,7 +78,17 @@ class TestNaiveBayesClassifier(unittest.TestCase):
         c = naivebayes.NaiveBayesClassifer()
         c.fit(X_train, y_train)
         y_pred = c.predict(X_test)
-        print("Digits Accuracy : ", accuracy_score(y_test, y_pred))
+
+        images = np.reshape(X_test, (X_test.shape[0], 8, 8))
+        count = 0
+        for i, (y1, y2) in enumerate(zip(y_pred, y_test)):
+            if y1 == y2:
+                #print(y1, y2)
+                if count < 5:
+                    count += 1
+                    #plt.matshow(images[i])
+                    #plt.show()
+        print("Digits Accuracy: ", accuracy_score(y_test, y_pred))
 
 
 if __name__ == '__main__':
